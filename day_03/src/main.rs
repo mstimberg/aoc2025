a508a80b73467c68e0fd52d397a1ba6aae5b9e48
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2025-12-03 11:31:14 +0100

    Day 03, part 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,35 +1,47 @@
 use std::fs;
 
-fn joltage(bank: &str) -> i32 {
-    // highest number (ignoring the last one) -- for several numbers take the first one (inverse of default choice, therefore we revert)
+fn joltage(bank: &str) -> u64 {
+    let mut indices: Vec<usize> = vec![];
+    // highest number (ignoring the eleven last ones) -- for several numbers take the first one (inverse of default choice, therefore we revert)
     let first_idx = bank.len()
-        - 2
-        - bank[..bank.len() - 1]
+        - 12
+        - bank[..bank.len() - 11]
             .chars()
             .rev()
             .enumerate()
             .max_by_key(|(_, value)| *value)
             .map(|(idx, _)| idx)
             .unwrap();
-    // highest number following the first number
-    let second_idx = bank.len()
-        - (1 + bank[first_idx + 1..]
+    indices.push(first_idx);
+
+    for digit in 1..12 {
+        let start = indices[digit - 1] + 1;
+        let stop = bank.len() - 12 + digit;
+        // highest number following the previous number
+        let local_idx = bank[start..stop + 1]
             .chars()
             .rev()
             .enumerate()
             .max_by_key(|(_, value)| *value)
             .map(|(idx, _)| idx)
-            .unwrap());
-    let first_value = bank[first_idx..first_idx + 1].parse::<i32>().unwrap();
-    let second_value = bank[second_idx..second_idx + 1].parse::<i32>().unwrap();
+            .unwrap();
+        let next_idx = stop - local_idx;
+        indices.push(next_idx);
+    }
 
-    first_value * 10 + second_value
+    let mut value: u64 = 0;
+    for (digit, index) in indices.into_iter().enumerate() {
+        value += 10_u64.pow(11 - TryInto::<u32>::try_into(digit).unwrap())
+            * bank[index..index + 1].parse::<u64>().unwrap();
+    }
+    println!("{}: {}", bank, value);
+    value
 }
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
     let banks = contents.lines();
     let joltages: Vec<_> = banks.map(|b| joltage(b)).collect();
-    let sum = joltages.into_iter().sum::<i32>();
+    let sum = joltages.into_iter().sum::<u64>();
     println!("Total sum: {}", sum);
 }
