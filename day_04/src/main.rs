b916ebf37f9c34f95949b9839d143b3d8f6ed892
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2025-12-04 10:43:40 +0100

    Day 04, part 1

---
+++ day_04/src/main.rs
@@ -0,0 +1,61 @@
+use ndarray::prelude::*;
+use std::{fs, iter::zip};
+
+fn print_map(map: &Array2<bool>) {
+    for row in map.outer_iter() {
+        for tile in row {
+            print!("{}", if *tile { '@' } else { '.' });
+        }
+        println!()
+    }
+}
+
+fn accessible_rolls(map: &Array2<bool>) -> u32{
+    let x_neighbours = [-1, -1, -1, 0, 0, 1, 1, 1];
+    let y_neighbours = [-1, 0, 1, -1, 1, -1, 0, 1];
+    let map_shape = map.shape();
+    let mut count = 0;
+    for index in 0..map_shape[0]*map_shape[1] {
+        let row = index / map_shape[1];
+        let col = index % map_shape[1];
+        if !map[[row, col]] {
+            continue;
+        }
+        let mut neighbours = 0;
+        for (dx, dy) in zip(x_neighbours, y_neighbours) {
+            let x: i32 = row as i32 + dx;
+            let y: i32 = col as i32 + dy;
+            if x >= 0 && y >= 0 && x < map_shape[0] as i32 && y < map_shape[1] as i32 && map[[x as usize, y as usize]] {
+                neighbours += 1;
+            }
+        }
+        if neighbours < 4 {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+
+    let mut rows = 0;
+    let mut flat_map = Vec::new();
+    for line in contents.lines() {
+        if line.is_empty() {
+            continue;
+        }
+        flat_map.extend(line.chars());
+        rows += 1;
+    }
+    let cols = flat_map.len() / rows;
+    assert!(cols * rows == flat_map.len());
+    let map = Array2::from_shape_vec(
+        (rows, cols),
+        flat_map.iter().map(|c| *c == '@').collect::<Vec<_>>(),
+    )
+    .unwrap();
+    // print_map(&map);
+    let count = accessible_rolls(&map);
+    println!("Number of accessible rolls: {}", count);
+}
