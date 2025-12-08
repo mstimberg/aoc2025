232a30062d39abc95c256a67ca579046c30f7b07
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2025-12-09 00:02:37 +0100

    Day 07, part 2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,7 +1,34 @@
-use std::fs;
+use std::{collections::HashMap, fs};
+
+fn timelines(lines: Vec<&str>, beam_pos: usize, cache: &mut HashMap<(usize, usize), u64>) -> u64 {
+    if lines.is_empty() {
+        return 1;
+    }
+    let key = (lines.len(), beam_pos);
+    if cache.contains_key(&key) {
+        return *cache.get(&key).unwrap();
+    }
+    let paths;
+    let line = lines[0];
+    if line[beam_pos..beam_pos+1] == *"." {
+        paths = timelines(lines[1..].to_vec(), beam_pos, cache);
+    } else { // split
+        let left_timelines = timelines(lines[1..].to_vec(), beam_pos - 1, cache);
+        let right_timelines = timelines(lines[1..].to_vec(), beam_pos + 1, cache);
+        paths = left_timelines + right_timelines;
+    }
+    cache.insert(key, paths);
+    return paths;
+}
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+    let lines: Vec<_> = contents.lines().collect();
+    let start = lines[0].find('S').unwrap();
+    let mut cache = HashMap::<(usize, usize), u64>::new();
+    let all_timelines = timelines(lines[1..].to_vec(), start, &mut cache);
+    println!("Timelines: {}", all_timelines);
+    // println!("Cache: {:?}", cache);
     let mut beams = vec![];
     let mut splits = 0;
     for line in contents.lines() {
