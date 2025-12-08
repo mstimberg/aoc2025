68ec10f6b01914e9143de8c6a33d7e9ba08c74d3
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2025-12-08 22:42:57 +0100

    Day 06, part 2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,7 +1,29 @@
 use std::fs;
 
+fn turned(text: String) -> Vec<Vec<char>> {
+    let mut first_line = true;
+    let mut turned_around: Vec<Vec<_>> = vec![];
+    for line in text.lines() {
+        if first_line {
+            for c in line.chars() {
+                turned_around.push(vec![c]);
+            }
+            first_line = false;
+        } else {
+            for (index, c) in line.chars().enumerate() {
+                turned_around[index].push(c);
+            }
+        }
+    }
+    // We add an empty vector in the end, so that every block ends with one
+    turned_around.push(vec![' ']);
+    turned_around
+}
+
+
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+
     let mut values_per_column: Vec<Vec<u64>> = vec![];
     let mut first_line = true;
     let mut results: Vec<_> = vec![];
@@ -18,7 +40,6 @@ fn main() {
             }
             first_line = false;
         } else {  // Last line with operators
-            println!("Length of line: {}", line.len());
             if line[0..1] == *"+" || line[0..1] == *"*"  { // last line
                 for (index, val) in line.split_whitespace().enumerate() {
                     let values = &values_per_column[index];
@@ -38,4 +59,32 @@ fn main() {
     }
     let total = results.iter().sum::<u64>();
     println!("Grand total: {}", total);
+
+    // Part 2
+    let contents2 = turned(contents.clone());
+    let mut results2 = vec![];
+    let mut new_calculation = true;
+    let mut current_operation = '+';
+    let mut values = vec![];
+    for column in contents2 {
+        if new_calculation {
+            current_operation = column[column.len()-1];
+            values.clear();
+            new_calculation = false;
+        }
+        let combined: String = column[0..column.len()-1].iter().collect::<String>().trim().to_string();
+        if combined.len() > 0 {
+            values.push(combined.parse::<u64>().unwrap());
+        } else {
+            // empty column: do the calculation and start again
+            if current_operation == '*' {
+                results2.push(values.iter().fold(1u64, |acc, el| acc * *el));
+            } else {
+                results2.push(values.iter().sum::<u64>());
+            }
+            new_calculation = true;
+        }
+    }
+    let total2 = results2.iter().sum::<u64>();
+    println!("Grand total 2: {}", total2);
 }
